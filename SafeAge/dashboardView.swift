c78ef5131fbdebd6101a4b876afd7787af11fc65
anyagupta15 <anya.gupta.1573@gmail.com>
2024-02-25 11:43:24 +0530

    dashboardView added

---
+++ SafeAge/dashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  dashboardView.swift
+//  SafeAge
+//
+//  Created by user1 on 23/02/24.
+//
+
+import SwiftUI
+
+struct dashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dashboardView()
+}
