91721aa00c44bdf5edb156ac2c55aedb143aadb8
anyagupta15 <anya.gupta.1573@gmail.com>
2024-03-15 22:35:56 +0530

    login and logout

---
+++ SafeAge/LogoutView.swift
@@ -0,0 +1,18 @@
+//
+//  LogoutView.swift
+//  SafeAge
+//
+//  Created by user1 on 15/03/24.
+//
+
+import SwiftUI
+
+struct LogoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogoutView()
+}
