c17f2cd6aa7c491bb06b9cea275d5b1df75c48f5
anyagupta15 <anya.gupta.1573@gmail.com>
2024-03-18 00:25:24 +0530

    healthkit-1

---
+++ SafeAge/Healthfirebase/newhealth.swift
@@ -0,0 +1,18 @@
+//
+//  newhealth.swift
+//  SafeAge
+//
+//  Created by user1 on 18/03/24.
+//
+
+import SwiftUI
+
+struct newhealth: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newhealth()
+}
